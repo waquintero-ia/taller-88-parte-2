3368a7455960838fabc5cf2c306f1a72e7b34067
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2023-05-09 22:04:57 -0500

    parte 2 taller 8
    parte 2 taller 8

---
+++ app/src/main/java/com/tutorialesprogramacionya/proyecto055/MainActivity.java
@@ -0,0 +1,49 @@
+package com.tutorialesprogramacionya.proyecto055;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imagen1;
+    private EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imagen1=(ImageView)findViewById(R.id.imageView);
+        et1=(EditText)findViewById(R.id.editText);
+
+    }
+
+    public void tomarFoto(View v) {
+        Intent intento1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File foto = new File(getExternalFilesDir(null), et1.getText().toString());
+        intento1.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
+        startActivity(intento1);
+    }
+
+    public void recuperarFoto(View v) {
+        Bitmap bitmap1 = BitmapFactory.decodeFile(getExternalFilesDir(null)+"/"+et1.getText().toString());
+        imagen1.setImageBitmap(bitmap1);
+    }
+
+    public void ver(View v) {
+        Intent intento1=new Intent(this,Actividad2.class);
+        startActivity(intento1);
+    }
+
+}
